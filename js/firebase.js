bf2103f0b248071ec73b0f83961845f5a92d2eae
derwan29 <55685261+derwan29@users.noreply.github.com>
2025-12-16 15:13:28 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,9 +3,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT.firebaseapp.com",
-  projectId: "PROJECT_ID"
+  apiKey: "AIzaSyCwBweYth4f-SnFLNqoh9Sw2JjE19nNLuA",
+  authDomain: "peminjaman-barang-687a3.firebaseapp.com",
+  projectId: "peminjaman-barang-687a3",
+  storageBucket: "peminjaman-barang-687a3.firebasestorage.app",
+  messagingSenderId: "420809674682",
+  appId: "1:420809674682:web:7dcf78e1961c97c5715783",
+  measurementId: "G-2E9J737GX1"
 };
 
 export const app = initializeApp(firebaseConfig);
